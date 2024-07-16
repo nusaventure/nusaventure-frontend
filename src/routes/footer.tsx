f95e24a1e2d5246f2f7db911dd913a958b1ded0e
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-16 22:49:19 +0700

    feat: footer with background image

--- src/routes/footer.tsx
+++ src/routes/footer.tsx
@@ -1,33 +1,15 @@
-import beachBall from "../images/footer/beachball.png";
-import umbrella from "../images/footer/umbrella.png";
-import sandal from "../images/footer/sandal.png";
-
-import island from "../images/footer/island.png";
-
 export function Footer() {
   return (
-    <footer className="relative p-4 bg-amber-200">
+    <footer className="relative p-4 pt-12 bg-footer bg-no-repeat bg-cover bg-bottom">
       <div className="relative z-10 flex flex-col items-center">
-        <img src={island} alt="island" className="h-12 w-12 mb-2" />
-        <p className="text-center">NUSAVENTURE</p>
+        <img
+          src="/images/footer/island.png"
+          alt="island"
+          className="h-12 w-12 mb-2"
+        />
+        <p className="text-center">Nusaventure</p>
       </div>
-      <div className="absolute inset-x-0 bottom-0 h-full bg-footer-pattern bg-no-repeat bg-bottom">
-        <img
-          src={beachBall}
-          alt="beach ball"
-          className="absolute bottom-4 left-4 h-12 w-12"
-        />
-        <img
-          src={umbrella}
-          alt="umbrella"
-          className="absolute bottom-4 right-4 h-12 w-12"
-        />
-        <img
-          src={sandal}
-          alt="sandal"
-          className="absolute bottom-4 left-20 h-12 w-12"
-        />
-      </div>
+
       <div className="text-center mt-8">
         <p>&copy; Nusaventure 2024</p>
       </div>
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,9 +5,11 @@ export function RootRoute() {
   return (
     <div className="flex flex-col min-h-screen">
       <header>{/* Header content */}</header>
+
       <main className="flex-grow">
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
