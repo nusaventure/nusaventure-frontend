cb135fbb092dc84902924a67c82dc9e1acb10957
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-07-26 21:37:13 +0700

    feat: add `removeAccessToken` to access token libs

--- src/libs/access-token.ts
+++ src/libs/access-token.ts
@@ -3,7 +3,11 @@ import Cookies from "js-cookie";
 const COOKIE_NAME = "access-token-nusaventure";
 
 export function setAccessToken(token: string) {
-  Cookies.set(COOKIE_NAME, token);
+  Cookies.set(COOKIE_NAME, token, { sameSite: "None", secure: true });
+}
+
+export function removeAccessToken() {
+  Cookies.remove(COOKIE_NAME, { sameSite: "None", secure: true });
 }
 
 export function getAccessToken() {
