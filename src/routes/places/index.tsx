16ce074aefbc663420f84f49ce38876e3db9fb19
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-07-26 21:46:01 +0700

    feat: simplify the codes and fix image size issue

--- src/routes/places/index.tsx
+++ src/routes/places/index.tsx
@@ -1,72 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Map, { MapRef, PointLike, Source } from "react-map-gl";
-import {
-  Form,
-  LoaderFunctionArgs,
-  useLoaderData,
-  useSearchParams,
-} from "react-router-dom";
-import { Link } from "react-router-dom";
-import "mapbox-gl/dist/mapbox-gl.css";
-import Layer from "react-map-gl/dist/esm/components/layer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FeaturedPlace, Place } from "@/types/places";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { MapRef, PointLike, Source } from "react-map-gl";
+import Layer from "react-map-gl/dist/esm/components/layer";
+import {
+  Form,
+  Link,
+  LoaderFunctionArgs,
+  useLoaderData,
+} from "react-router-dom";
 
-import api from "@/libs/api";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import api from "@/libs/api";
 import NusaVentureLogo from "/images/places/nusa-venture-black.svg";
 
-type responsePlaces = {
-  data: Array<{
-    id: string;
-    title: string;
-    latitude: number;
-    longitude: number;
-    imageUrl: string;
-    description: string;
-    address: string;
-    categories: Array<{
-      id: string;
-      name: string;
-      slug: string;
-    }>;
-  }>;
-};
+type responsePlaces = { data: Array<Place> };
 
-type Places = {
-  id: string;
-  title: string;
-  latitude: number;
-  longitude: number;
-  imageUrl: string;
-  description: string;
-  address: string;
-  categories: Array<{
-    id: string;
-    name: string;
-    slug: string;
-  }>;
-};
-
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const keyword = new URL(request.url).searchParams.get("q");
 
-  const responsePlaces = await api<responsePlaces>(
-    `places?search=${keyword ?? ""}`
-  );
+  const [responsePlaces, responseTopDestinations] = await Promise.all([
+    api<responsePlaces>(`places?search=${keyword ?? ""}`),
+    api<{ data: Array<FeaturedPlace> }>("/places/featured"),
+  ]);
 
   return {
     keyword: keyword ?? "",
     places: responsePlaces.data,
+    topDestinations: responseTopDestinations.data,
   };
 }
 
 export function PlacesIndexRoute() {
-  const { places } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const { places, keyword } = useLoaderData() as Awaited<
+    ReturnType<typeof loader>
+  >;
 
   const mapRef = useRef<MapRef>(null);
 
@@ -176,7 +150,7 @@ export function PlacesIndexRoute() {
       <aside className="w-[720px] h-screen flex flex-col">
         <PlacesSidebarHeader />
         <div className="p-6 h-[85%]">
-          <PlaceDetailPlaceholder places={places} />
+          <PlaceDetailPlaceholder places={places} keyword={keyword} />
         </div>
       </aside>
 
@@ -211,17 +185,6 @@ export function PlacesIndexRoute() {
 
 function PlacesSidebarHeader() {
   const { keyword } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
-
-  useEffect(() => {
-    if (query) {
-      const searchInput = document.getElementById("search") as HTMLInputElement;
-      if (searchInput) {
-        searchInput.value = query;
-      }
-    }
-  }, [query]);
 
   return (
     <header className="px-6 py-4 flex justify-between items-center gap-6">
@@ -248,32 +211,33 @@ function PlacesSidebarHeader() {
   );
 }
 
-function PlaceDetailPlaceholder({ places }: { places: Places[] }) {
-  const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
-
+function PlaceDetailPlaceholder({
+  places,
+  keyword,
+}: {
+  places: Place[];
+  keyword: string;
+}) {
   return (
     <div className="h-[100%]">
-      <p className="font-medium text-xl mb-6">Show result of "{query}"</p>
+      <p className="font-medium text-xl mb-6">Show result of "{keyword}"</p>
 
-      <ScrollArea className="h-[100%] pb-10">
+      <ScrollArea className="h-[100%]">
         {places.map((place, index) => (
           <div
             className="flex flex-row gap-4 mb-4 min-h-[145px] w-full"
             key={index}
           >
             <img
-              width="198px"
-              height="145px"
               className="object-cover rounded-lg w-[198px] h-[145px]"
               src={place.imageUrl}
               alt={place.title}
             />
 
-            <div className="flex flex-col gap-2 flex-1">
+            <div className="flex flex-col gap-2 flex-1 overflow-hidden">
               <p className="text-xl font-bold">{place.title}</p>
 
-              <div className="flex flex-row gap-4">
+              <div className="flex flex-row gap-4 ">
                 {place.categories.map((category, index) => (
                   <div
                     key={index}
@@ -286,10 +250,10 @@ function PlaceDetailPlaceholder({ places }: { places: Places[] }) {
                 ))}
               </div>
 
-              <p className="text-sm font-medium text-gray-500 truncate text-ellipsis w-[360px]">
+              <p className="text-sm font-medium text-gray-500 truncate text-ellipsis">
                 {place.description}
               </p>
-              <p className="text-sm font-medium text-gray-500 truncate text-ellipsis w-[360px]">
+              <p className="text-sm font-medium text-gray-500 truncate text-ellipsis">
                 {place.address}
               </p>
             </div>
