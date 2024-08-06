687370b00a314db2d266931da54e57e2dd32ee99
M Haidar Hanif <git@mhaidarhanif.com>
2024-08-07 00:09:38 +0700

    feat: user navigation avatar image url

--- src/components/user-navigation.tsx
+++ src/components/user-navigation.tsx
@@ -1,5 +1,7 @@
+import { Form, Link } from "react-router-dom";
+
 import { authProvider } from "@/libs/auth";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,50 +9,44 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Form, Link } from "react-router-dom";
+} from "@/components/ui/dropdown-menu";
 
 export function UserNavigation() {
   const user = authProvider.user;
+  if (!user) return null;
+
+  const userAvatarImageURL = `https://api.dicebear.com/9.x/lorelei-neutral/svg?seed=${user.username}&backgroundType=gradientLinear`;
 
   return (
-    <>
-      {user ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger className="outline-none">
-            <Avatar>
-              <AvatarImage
-                src={`https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=${user.username}&backgroundType=gradientLinear`}
-              />
-              <AvatarFallback>{user.username}</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>
-              <div>{`${user.firstName} ${user.lastName}`}</div>
-              <div className="text-gray-500 text-sm">@{user.username}</div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <Link to="/dashboard">
-              <DropdownMenuItem className="cursor-pointer">
-                Dashboard
-              </DropdownMenuItem>
-            </Link>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="outline-none">
+        <Avatar>
+          <AvatarImage src={userAvatarImageURL} />
+          <AvatarFallback>{user.username}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          <div>{`${user.firstName} ${user.lastName}`}</div>
+          <div className="text-gray-500 text-sm">@{user.username}</div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link to="/dashboard">
+          <DropdownMenuItem className="cursor-pointer">
+            Dashboard
+          </DropdownMenuItem>
+        </Link>
 
-            <DropdownMenuSeparator />
+        <DropdownMenuSeparator />
 
-            <Form method="post" action="/" className="inline">
-              <button name="intent" value="logout" className="h-full w-full">
-                <DropdownMenuItem className="cursor-pointer">
-                  Logout
-                </DropdownMenuItem>
-              </button>
-            </Form>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        ""
-      )}
-    </>
+        <Form method="post" action="/" className="inline">
+          <button name="intent" value="logout" className="h-full w-full">
+            <DropdownMenuItem className="cursor-pointer">
+              Logout
+            </DropdownMenuItem>
+          </button>
+        </Form>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
